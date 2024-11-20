448643afe1bbaac8b635d2f3e7116ed3c3170d64
Wisnu Nugroho <wisnunugroho614@gmail.com>
2024-11-20 10:58:48 +0700

    fix: wrong port

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -56,7 +56,7 @@ class ItemCard extends StatelessWidget {
           } else if (item.name == "Logout") {
               final response = await request.logout(
                   // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-                  "http://localhost:800/auth/logout/");
+                  "http://localhost:8000/auth/logout/");
               String message = response["message"];
               if (context.mounted) {
                   if (response['status']) {
